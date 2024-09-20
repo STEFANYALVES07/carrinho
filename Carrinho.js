91ef0e47645c235806f3028323869d3723ec1864
Stefany Alves <stefanyalves0706@gmail.com>
2024-09-20 15:37:24 -0300

    commit

---
+++ Carrinho.js
@@ -0,0 +1,13 @@
+import { Text } from "react-native-web";
+
+export default function Carrinho() {
+  return;
+  <Text>teeeee</Text>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
